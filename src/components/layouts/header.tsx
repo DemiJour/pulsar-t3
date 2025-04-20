774e818f66e52aa40b906ef21d003b6192078bda
Jour <whiteshadow0418@gmail.com>
2025-04-20 12:22:56 -0600

    fix: add the chevron down mark

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -7,8 +7,8 @@ export default function HeaderPage() {
           <img src="/logos/Logotype.svg" alt="" className="pl-[42px]" />
           <div className="flex py-1 gap-8">
             <div className="text-white font-medium">Home</div>
-            <div className="text-white font-medium">Products</div>
-            <div className="text-white font-medium">Resources</div>
+            <div className="text-white font-medium flex gap-2">Products <img src="/icons/chevron-down.svg" className="w-5 h-5" alt="" /></div>
+            <div className="text-white font-medium flex gap-2">Resources <img src="/icons/chevron-down.svg" className="w-5 h-5" alt="" /></div>
             <div className="text-white font-medium">Pricing</div>
           </div>
         </div>
